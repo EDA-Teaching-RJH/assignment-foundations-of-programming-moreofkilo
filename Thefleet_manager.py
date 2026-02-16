24c04899a167cd39e193bced59e97836ff878802
moreofkilo <ro13@kent.ac.uk>
2026-02-16 10:13:59 +0000

    remove member

--- Thefleet_manager.py
+++ Thefleet_manager.py
@@ -1,30 +1,16 @@
-def add_member(names, ranks, divs, ids):
-    print("\n-- Add New Crew --")
+def remove_member(names, ranks, divs, ids):
+    print("\n-- Remove Crew --")
+    target = input("Enter ID: ").upper()
 
-    new_id = input("Assign ID: ").upper()
-
-    if new_id in ids:
-        print("ERROR: ID already exists.")
+    if target not in ids:
+        print("Crew ID not located.")
         return
 
-    valid_ranks = [
-        "Captain", "Commander",
-        "Lieutenant Commander",
-        "Lieutenant", "Ensign", "Admiral"
-    ]
+    pos = ids.index(target)
 
-    new_rank = input("Rank: ")
+    del names[pos]
+    del ranks[pos]
+    del divs[pos]
+    del ids[pos]
 
-    if new_rank not in valid_ranks:
-        print("ERROR: Invalid Starfleet rank.")
-        return
-
-    new_name = input("Name: ")
-    new_div = input("Division: ")
-
-    names.append(new_name)
-    ranks.append(new_rank)
-    divs.append(new_div)
-    ids.append(new_id)
-
-    print("Crew member successfully added.")
+    print("Crew member deleted.")
