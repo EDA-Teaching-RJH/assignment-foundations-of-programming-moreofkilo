c04b8af0bc87cf897350d7ba80591a876002d9e9
moreofkilo <ro13@kent.ac.uk>
2026-02-16 10:20:17 +0000

    count officer

--- Thefleet_manager.py
+++ Thefleet_manager.py
@@ -1,17 +1,6 @@
-def calculate_payroll(ranks):
-    pay = {
-        "Admiral": 1500,
-        "Captain": 1000,
-        "Commander": 800,
-        "Lieutenant Commander": 600,
-        "Lieutenant": 400,
-        "Ensign": 200
-    }
-
-    total_cost = 0
-
+def count_officers(ranks):
+    total = 0
     for r in ranks:
-        if r in pay:
-            total_cost += pay[r]
-
-    return total_cost
+        if r in ("Captain", "Commander"):
+            total += 1
+    return total
